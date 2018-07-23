e643ff9c49209f95f2a80ca2a893eea50765dc51
Pat Walls <patrickswalls@gmail.com>
2018-07-23 19:37:47 -0400

    add date grouping

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,3 +1,11 @@
 class Run < ApplicationRecord
   belongs_to :user
+
+  def self.group_by_day
+    all.group_by(&:group_by_criteria)
+  end
+
+  def group_by_criteria
+    created_at.to_date.to_s(:db)
+  end
 end
